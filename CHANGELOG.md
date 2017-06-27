c37c70eabc982250626cbf632270b1b56c8ccabd
Garth Hitchens <ghitchens@me.com>
2017-06-27 15:35:24 -0700

    Added changelog and updated mix.exs to reflect version changes

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# CHANGELOG
+
+## 0.2.4-dev (2017-June-27)
+
+- Updated to use latest HTTPotion and fixed API usage
+- Updated to use latest Conform and fixed API usage
+- Fixed major compile errors (but not warnings) under elixir 1.4
+
+## 0.2.1  (sometime late 2015)
--- mix.exs
+++ mix.exs
@@ -2,13 +2,15 @@ defmodule CellTool.Mixfile do
 
   use Mix.Project
 
-  def project do
-    [app: :celltool,
-     escript: [main_module: CellTool, name: "cell", path: "/usr/local/bin/cell"],
-     version: "0.2.4",
-     elixir: "~> 1.4",
-     deps: deps()]
-  end
+  def project, do [
+    app: :celltool,
+    escript: [
+      main_module: CellTool, name: "cell", path: "/usr/local/bin/cell"
+    ],
+    version: "0.2.4-dev",
+    elixir: "~> 1.4",
+    deps: deps()
+  ]
 
   def application, do: [
      applications: [:logger]
